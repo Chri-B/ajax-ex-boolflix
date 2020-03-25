80609c229f93d2ff92c1c5be774e6acc297db32f
Christian Beretta <beretta.christian2602@gmail.com>
2020-03-25 16:58:07 +0100

    inizio milestone 5

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
     var cardSrc = $("#card-template").html();
     var cardTemplate = Handlebars.compile(cardSrc);
 
+    var cardCastSrc = $('#actor-template').html();
+    var cardCastTemplate = Handlebars.compile(cardCastSrc);
+
     // all'input di ricerca viene aggiunto un effetto box-shadow
     $('#input-search').click(function() {
         $(this).css('box-shadow', 'inset 0px 0px 5px 2px #d71d29');
@@ -30,13 +33,29 @@ $(document).ready(function() {
     // al mouse hover su una carta viene nascosta la copertina e vengono visualizzate le info della carta
     $('.container').on( 'mouseenter', '.card', function() {
         $(this).children('img').hide();
-        $(this).children('.card-text').addClass('active');
+        $(this).children('.card-text').show();
     });
     // al mouse leave viene ripristinata la situazione iniziale
     $('.container').on( 'mouseleave', '.card', function() {
+        $(this).parents('.more-info').hide();
         $(this).children('img').show();
-        $(this).children('.card-text').removeClass('active');
+        $(this).children('.card-text').hide();
+        $(this).find('.box-generi').hide();
+        $(this).find('.more-info').remove();
     });
+
+    $('section').on('click', '.card button', function() {
+        var movieId = $(this).parents('.card').data('movieId');
+        $(this).parents('.card-text').hide();
+        var that = $(this);
+        that.parents('.card-text').siblings('.box-generi').show();
+        console.log(movieId);
+        getCastGenreApi(movieId, that);
+    });
+
+    // --------------------------------------------------------------------------------------------------
+    // ------------------------------------------FUNZIONI------------------------------------------------
+    // --------------------------------------------------------------------------------------------------
 
     // funzione che restituisce i film ricercati
     function findMedia() {
@@ -55,7 +74,7 @@ $(document).ready(function() {
         return val;
     };
 
-    // funzione per raggiungere il film nell'API
+    // funzione per raggiungere i film/serie tv nell'API
     function getMediaApi(tipo, inpSearch, whereAppend) {
         var apiBaseUrl = 'https://api.themoviedb.org/3';
         $.ajax({
@@ -103,6 +122,51 @@ $(document).ready(function() {
             $(whereAppend).append(cardMovie);
         }
     }
+
+    // funzione per raggiungere i dati relativi a Genere e Cast
+    function getCastGenreApi(idFilmSerie, selezionato) {
+        var apiBaseUrl = 'https://api.themoviedb.org/3';
+        $.ajax({
+            url: apiBaseUrl + '/movie/' + idFilmSerie,
+            method: 'GET',
+            data: {
+                api_key: 'bbf583179ea7e0b62001a8dd43710a73',
+                language: 'it-IT',
+                append_to_response: 'credits'
+            },
+            success: function(data) {
+                var castMovie = data.credits.cast;
+                for (var i = 0; i < 5; i++) {
+                    // console.log(castMovie[i]);
+                    var actor = castMovie[i];
+                    var castTemplate = {
+                        foto: actor.profile_path,
+                        personaggio: actor.character,
+                        attore: actor.name
+                    }
+                    var cardCast = cardCastTemplate(castTemplate);
+                    $(selezionato).parents('.card').append(cardCast);
+                    console.log(castTemplate.foto);
+                }
+                createBoxGenre(selezionato, data);
+            },
+            error: function(err) {
+                alert('Errore richiesta Cast');
+            }
+        });
+    };
+
+    // funzione che ricava i generi del film e li assembla in un array
+    function createBoxGenre(selezionato, data) {
+        var genreMovie = data.genres;
+        var arrayGenre = [];
+        for (var i = 0; i < genreMovie.length; i++) {
+            arrayGenre.push(genreMovie[i].name);
+        }
+        var arrayJoinGenre = arrayGenre.join(', ');
+        $(selezionato).parents('.card').find('.box-generi .generi span').remove();
+        $(selezionato).parents('.card').find('.box-generi .generi').append('<span>' + arrayJoinGenre + '</span>');
+    };
 
     // funzione che restituisce una immagine di bandiera nella sezione lingua originale
     function flagsImg(country) {
